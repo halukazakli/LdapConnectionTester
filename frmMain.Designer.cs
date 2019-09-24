c4514a96d50682ec4a9853dec56ad60537b169e9
Haluk Azaklı <haluk.azakli@ifs.com.tr>
2019-09-24 17:56:47 +0300

    Added project files

---
+++ frmMain.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace LdapConnectionTester
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbAuthType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudProtokolVersion = new System.Windows.Forms.NumericUpDown();
+            this.chkSsl = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCertFile = new System.Windows.Forms.TextBox();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.LinkLabel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.nudProtokolVersion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auth Type:";
+            // 
+            // cbAuthType
+            // 
+            this.cbAuthType.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cbAuthType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbAuthType.FormattingEnabled = true;
+            this.cbAuthType.Location = new System.Drawing.Point(196, 31);
+            this.cbAuthType.Name = "cbAuthType";
+            this.cbAuthType.Size = new System.Drawing.Size(121, 21);
+            this.cbAuthType.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(320, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Protocol Version:";
+            // 
+            // nudProtokolVersion
+            // 
+            this.nudProtokolVersion.Location = new System.Drawing.Point(323, 32);
+            this.nudProtokolVersion.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudProtokolVersion.Name = "nudProtokolVersion";
+            this.nudProtokolVersion.Size = new System.Drawing.Size(84, 20);
+            this.nudProtokolVersion.TabIndex = 2;
+            this.nudProtokolVersion.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // chkSsl
+            // 
+            this.chkSsl.AutoSize = true;
+            this.chkSsl.Location = new System.Drawing.Point(13, 69);
+            this.chkSsl.Name = "chkSsl";
+            this.chkSsl.Size = new System.Drawing.Size(126, 17);
+            this.chkSsl.TabIndex = 3;
+            this.chkSsl.Text = "Secure Socket Layer";
+            this.chkSsl.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Cert File:";
+            // 
+            // txtCertFile
+            // 
+            this.txtCertFile.Location = new System.Drawing.Point(13, 109);
+            this.txtCertFile.Name = "txtCertFile";
+            this.txtCertFile.ReadOnly = true;
+            this.txtCertFile.Size = new System.Drawing.Size(394, 20);
+            this.txtCertFile.TabIndex = 4;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(13, 31);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(177, 20);
+            this.txtServer.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Server:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(332, 210);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.AutoSize = true;
+            this.btnOpenFile.Location = new System.Drawing.Point(387, 112);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(16, 13);
+            this.btnOpenFile.TabIndex = 9;
+            this.btnOpenFile.TabStop = true;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.btnOpenFile_LinkClicked);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(13, 210);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(13, 162);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(202, 20);
+            this.txtUsername.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Username:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(230, 162);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(177, 20);
+            this.txtPassword.TabIndex = 13;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(227, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Password:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 249);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCertFile);
+            this.Controls.Add(this.chkSsl);
+            this.Controls.Add(this.nudProtokolVersion);
+            this.Controls.Add(this.cbAuthType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = global::LdapConnectionTester.Properties.Resources.iconfinder_connect_45541;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ldap Connection Tester";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudProtokolVersion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbAuthType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudProtokolVersion;
+        private System.Windows.Forms.CheckBox chkSsl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCertFile;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.LinkLabel btnOpenFile;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices.Protocols;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+
+namespace LdapConnectionTester
+{
+    public partial class frmMain : Form
+    {
+        static string _server = "";
+        static AuthType _authType = AuthType.Ntlm;
+        static int _protocolVersion = 3;
+        static bool _ssl = false;
+        static string _certFile = "";
+        static string _user = "";
+        static string _pass = "";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            PopulateAuthTypes();
+        }
+
+        void PopulateAuthTypes()
+        {
+            try
+            {
+                int index = -1;
+                cbAuthType.Items.Clear();
+                foreach (var item in Enum.GetValues(typeof(AuthType)).Cast<AuthType>())
+                {
+                    index++;
+                    cbAuthType.Items.Add(item);
+
+                    if (item == AuthType.Ntlm)
+                        cbAuthType.SelectedIndex = index;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("[PopulateAuthTypes] " + ex.Message);
+            }
+        }
+
+       
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _server = txtServer.Text;
+                _authType = (AuthType)cbAuthType.SelectedItem;
+                _protocolVersion = Convert.ToInt32(nudProtokolVersion.Value);
+                _ssl = chkSsl.Checked;
+                _certFile = txtCertFile.Text;
+
+                Cursor.Current = Cursors.WaitCursor;
+                var connected = Connect();
+                Cursor.Current = Cursors.Default;
+
+                MessageBox.Show("Connected.");
+            }
+            catch (Exception ex)
+            {
+                Cursor.Current = Cursors.Default;
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        static bool Connect()
+        {
+            using (var ldapConnection = new LdapConnection(_server))
+            {
+                ldapConnection.AuthType = _authType;
+                ldapConnection.SessionOptions.ProtocolVersion = _protocolVersion;
+                ldapConnection.SessionOptions.SecureSocketLayer = _ssl;
+                
+                if (_ssl && !string.IsNullOrEmpty(_certFile))
+                    ldapConnection.SessionOptions.VerifyServerCertificate = new VerifyServerCertificateCallback(ServerCallback);
+                
+                ldapConnection.Credential = new NetworkCredential(_user, _pass);
+                ldapConnection.Bind();
+                return true;
+            }
+        }
+        
+        static bool ServerCallback(LdapConnection connection, X509Certificate certificate)
+        {
+            try
+            {
+                X509Certificate expectedCert = X509Certificate.CreateFromCertFile(_certFile);
+                if (expectedCert.Equals(certificate))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("[Certificate Error] " + ex.Message);
+                return false;
+            }
+        }
+
+        private void btnOpenFile_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var dialog = openFileDialog1.ShowDialog();
+            if (dialog == System.Windows.Forms.DialogResult.OK)
+            {
+                txtCertFile.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
